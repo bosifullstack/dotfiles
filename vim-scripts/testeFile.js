ba9c3ce36c428d333db44e454cfdc7cd3d41935d
Felipe Azevedo Bosi <felipeb@lumahealth.io>
2020-09-01 09:55:33 -0300

    vim-scripts

---
+++ vim-scripts/testeFile.js
@@ -0,0 +1,32 @@
+import { defineMessages } from 'react-intl.macro';
+
+export default defineMessages({
+  article: {
+    id: 'components.ActivityText.article',
+    defaultMessage: '',
+  },
+  articlePatient: {
+    id: 'components.ActivityText.articlePatient',
+    defaultMessage: 'Patient',
+  },
+  confirmTheir: {
+    id: 'components.ActivityText.confirmTheir',
+    defaultMessage: 'confirmed their',
+  },
+  appointmentWith: {
+    id: 'components.ActivityText.appointmentWith',
+    defaultMessage: 'appointment with',
+  },
+  at: {
+    id: 'components.ActivityText.at',
+    defaultMessage: 'at',
+  },
+  cancelledTheir: {
+    id: 'components.ActivityText.cancelledTheir',
+    defaultMessage: 'confirmed their',
+  },
+  viaLumaWebsite: {
+    id: 'components.ActivityText.viaLumaWebsite',
+    defaultMessage: 'via the Luma Health website',
+  },
+ "components.ActivityText.article"});
