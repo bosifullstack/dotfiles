013d4d881fc32fcec8f04d57bb67191d7c889892
Felipe Azevedo Bosi <felipeb@lumahealth.io>
2020-10-30 18:52:08 -0300

    Refactored rmtodoist

--- scripts/rmtodoist.sh
+++ scripts/rmtodoist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-todoist --color list | awk '{printf("%010d %s\n", NR, $0)}'
+todoist --color list | awk '{printf("\033[32m%010d \033[37m%s\n", NR, $0)}'
 
-echo "Input the line number of the task that you want to close"
+echo -e "\n\033[33mInput the line number of the task that you want to close"
 
 read lineNumber
 awkSettings="NR==$lineNumber"
